a7bf9aed5cf53e1726d55c1699a6ff732396f231
mdarrakibislamrifat <rohanrifat43@gmail.com>
2024-07-31 11:15:51 +0600

    Google sign in with oAuth and prisma initialize

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,10 +1,25 @@
+"use client";
+import { useSession, signIn } from "next-auth/react";
 import styles from "./loginPage.module.css";
+import { useRouter } from "next/navigation";
+const LoginPage = () => {
+  const { data, status } = useSession();
+  const router = useRouter();
 
-const page = () => {
+  if (status === "loading") {
+    return <div className={styles.loading}>Loading...</div>;
+  }
+
+  if (status === "authenticated") {
+    router.push("/");
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <div className={styles.socialButton}>Sign in with Google</div>
+        <div className={styles.socialButton} onClick={() => signIn("google")}>
+          Sign in with Google
+        </div>
         <div className={styles.socialButton}>Sign in with Github</div>
         <div className={styles.socialButton}>Sign in with Facebook</div>
       </div>
@@ -12,4 +27,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default LoginPage;
--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -2,6 +2,7 @@
 import Link from "next/link";
 import styles from "./authLinks.module.css";
 import { useState } from "react";
+import { signOut } from "next-auth/react";
 
 const AuthLinks = () => {
   const [open, setOpen] = useState(false);
@@ -19,7 +20,9 @@ const AuthLinks = () => {
           <Link href="/write" className={styles.link}>
             Write
           </Link>
-          <span className={styles.link}>Logout</span>
+          <span className={styles.link} onClick={signOut}>
+            Logout
+          </span>
         </>
       )}
       <div className={styles.burger} onClick={() => setOpen(!open)}>
@@ -37,7 +40,9 @@ const AuthLinks = () => {
           ) : (
             <>
               <Link href="/write">Write</Link>
-              <span className={styles.link}>Logout</span>
+              <span className={styles.link} onClick={() => signOut}>
+                Logout
+              </span>
             </>
           )}
         </div>
